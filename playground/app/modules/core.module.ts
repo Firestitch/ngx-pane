f6504f19bc25a8eca7890b1f45103dbb6ae93077
Raymond Gigliotti <ray@firestitch.com>
2019-05-31 08:55:26 -0400

    updated fsexample

---
+++ playground/app/modules/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FsPaneModule } from '@firestitch/pane';
+
+
+@NgModule({
+  imports: [
+    FsPaneModule.forRoot()
+  ],
+
+})
+export class CoreModule {
+}
