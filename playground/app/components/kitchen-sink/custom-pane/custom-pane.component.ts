26387752d3016bcb542a812d8846702f144ab3bc
Eugene Kubesh <codbasters@gmail.com>
2021-06-24 20:27:42 +0300

    remove dummy data

--- playground/app/components/kitchen-sink/custom-pane/custom-pane.component.ts
+++ playground/app/components/kitchen-sink/custom-pane/custom-pane.component.ts
@@ -5,10 +5,6 @@ import { PANE_DATA } from '@firestitch/pane';
   templateUrl: './custom-pane.component.html',
 })
 export class CustomPaneComponent {
-  public da = '';
   constructor(@Inject(PANE_DATA) public data: any) {
-    (window as any).cock = (val) => {
-      this.da = val;
-    }
   }
 }
