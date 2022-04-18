25b4f9bf888576f3e084e59401a530cf1b5afffb
Eugene Kubesh <codbasters@gmail.com>
2022-04-18 15:42:37 +0300

    add FsPaneComponent to exports

--- src/public_api.ts
+++ src/public_api.ts
@@ -7,3 +7,4 @@ export { FsPaneModule } from './app/fs-pane.module';
 export { FsPane } from './app/services/pane.service';
 export { PANE_DATA } from './app/services/pane-data';
 export { FsPaneStateChangeType, FsPaneStateChangeEvent } from './app/interfaces/state-change-event.interface';
+export { FsPaneComponent } from './app/components/pane/pane.component';
